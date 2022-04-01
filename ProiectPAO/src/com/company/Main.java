4e2eb06ae32191f2cf4716aa6a90e9d02c285e8d
stefdasca <stefdasca@gmail.com>
2022-04-01 20:07:48 +0300

    Etapa 1

---
+++ ProiectPAO/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        public class Main {
+            School scoala = new School();
+
+            ArrayList<Double> note;
+            ArrayList<String> hobby;
+
+            Students s1 = scoala.createStudent("Georgescu", "Alexandru", note, hobby);
+            Students s2 = scoala.createStudent("Popescu", "Maria", note, hobby);
+            Students s3 = scoala.createStudent("Coman", "Anca", note, hobby);
+            Students s4 = scoala.createStudent("Campeanu", "Radu", note, hobby);
+            Students s5 = scoala.createStudent("Blejan", "Bogdan", note, hobby);
+
+            Teachers t1 = scoala.createTeacher("Nistor", "Sergiu", 52, 5000, "Mate");
+            Teachers t2 = scoala.createTeacher("Dragan", "Maria", 48, 3800, "Engleza");
+
+        }
+
+    }
+}
